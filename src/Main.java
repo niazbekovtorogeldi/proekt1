d9b8e679c6967c96038220c79d8f1c5b10ab5327
USER <niazbekovtorogeldi5@gmail.com>
2023-01-26 16:37:42 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("азыр менде %d сом бар",1000);
+        System.out.printf("бугун сабак саат %f жарым башталат", 11.5);
+        System.out.printf("салам менин атым %s,%d жаштамын", "асан",22);
+
+        System.out.print("diego");
+        System.out.println("is the best");
+        System.out.print("amigo");
+        System.out.println("is the best");
+
+        System.out.printf("азыр менде %d сом бар",1000);
+        System.out.printf("бугун сабак саат %f жарым башталат", 11.5);
+        System.out.printf("салам менин атым %s,%d жаштамын", "асан",22);
+
+        System.out.print("салам\r менин атым асан\n,22 жаштамын\t ");
+        System.out.println("мен буюрса ");
+        System.out.println("\t\t\tтогуз айда ");
+        System.out.println("\t\t\t\t\t\t програмист болом");
+
+
+
+
+    }
+}
